0ae29c843931208f3de7da445f84989c6a13603f
sandeep kumar <sicsdev21@gmail.com>
2022-01-24 13:08:33 -0800

    create layout structure

--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -2,16 +2,15 @@ import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import { Grid, TextField, Select, Checkbox, Button } from "@mui/material";
-import Rectangle from "../images/Rectangle.png";
-import xcalalogo from "../../assets/logo.svg";
-import "./1.module.scss";
-import messagebox from "../../assets/regis-icon-2.svg";
-import vector from "../../assets/regis-icon-1.svg";
+// import Rectangle from "../Rectangle.png";
+// import xcalalogo from "../logo.svg";
+// import messagebox from "../regis-icon-2.svg";
+// import vector from "../regis-icon-1.svg";
+import styles from "./Register.module.scss";
 import { useState } from "react";
-import FormHelperText from "@mui/material/FormHelperText";
-import validator from "validator";
 import isEmail from "validator/lib/isEmail";
-    
+import "react-phone-number-input/style.css";
+
 function Firstpage() {
   const [name, setName] = useState("");
   const [motherslastname, setMothername] = useState("");
@@ -19,37 +18,85 @@ function Firstpage() {
   const [emailError, setEmailError] = useState(false);
   const [errormsg, setErrormsg] = useState("");
   const [activeinputcolor, setactiveinputcolor] = useState("");
- 
-  // const activeinputcolor = (e) => {
-  //   setactiveinputcolor(e.target.value);
-  //   if(name.length>=3){
-  //     setErrormsg("Algo anda mal... Verifica tu email")
-  //   }
-  // }
- 
+  const [helpermsg, setHelpermsg] = useState({
+    namehelpermsg: "",
+    mothernamehelpermsg: "",
+    lastnamehelpermsg: "",
+  });
 
+  //this is for name textfiled (Nombre)
   const onnamechange = (e) => {
     setName(e.target.value);
-
+    if (name.length >= 2) {
+      setHelpermsg((nametext) => {
+        return {
+          ...nametext,
+          namehelpermsg: "Perfecto",
+        };
+      });
+    }
+    if (name.length == 1) {
+      setHelpermsg((nametext) => {
+        return {
+          ...nametext,
+          namehelpermsg: "",
+        };
+      });
+    }
   };
 
+  //this is for motherlastname textfiled (Apellido materno)
   const onmothernamechange = (e) => {
     setMothername(e.target.value);
-  };
-  const onlastnamechange = (e) => {
-    setLastname(e.target.value);
+    if (motherslastname.length >= 2) {
+      setHelpermsg((mothernametext) => {
+        return {
+          ...mothernametext,
+          mothernamehelpermsg: "Muy bien",
+        };
+      });
+    }
+    if (motherslastname.length == 1) {
+      setHelpermsg((mothernametext) => {
+        return {
+          ...mothernametext,
+          mothernamehelpermsg: "",
+        };
+      });
+    }
   };
 
-  // email validation
-    const error = (e) => {
+  //this is for lastname textfiled (Apellido paterno)
+  const onlastnamechange = (e) => {
+    setLastname(e.target.value);
+    if (lastname.length >= 2) {
+      setHelpermsg((lastnametext) => {
+        return {
+          ...lastnametext,
+          lastnamehelpermsg: "Muy bien",
+        };
+      });
+    }
+    if (lastname.length == 1) {
+      setHelpermsg((lastnametext) => {
+        return {
+          ...lastnametext,
+          lastnamehelpermsg: "",
+        };
+      });
+    }
+  };
+
+  //this is for email
+  const error = (e) => {
     const val = e.target.value;
 
     if (isEmail(val)) {
       setEmailError(false);
-      setErrormsg("")
+      setErrormsg("");
     } else {
       setEmailError(true);
-      setErrormsg( "Algo anda mal... Verifica tu email")
+      setErrormsg("Algo anda mal... Verifica tu email");
       return;
     }
   };
@@ -57,32 +104,37 @@ function Firstpage() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <div className="register-wrapper">
+      <div className={styles.register_wrapper}>
         <Container maxWidth="sm">
           <Grid container maxWidth="sm" spacing={2}>
             <Grid item md={12}>
               <div className="xcala-logo">
-                <img src={xcalalogo} alt="logo"></img>
+                <img src="/assets/images/logo.svg" alt="logo"></img>
               </div>
             </Grid>
             <Grid item md={12} mb={3}>
-              <div className="rectangle-heading">
-                <img src={Rectangle} alt=""></img>
-                <p1>¡Únete a Xcala!</p1>
+              <div className={styles.rectangle_heading}>
+                <img src="/assets/images/Rectangle.png" alt=""></img>
+                <p>¡Únete a Xcala!</p>
               </div>
 
-              <div className="rectangle-p">
+              <div className={styles.rectangle_p}>
                 <p>Invierte seguro, invierte con propósito...</p>
               </div>
             </Grid>
           </Grid>
-          <div className="form-register">
-            <div className="form-inner">
-              <Grid container maxWidth="sm" spacing={2} className="bluebox">
+          <div className={styles.form_register}>
+            <div className={styles.form_inner}>
+              <Grid
+                container
+                maxWidth="sm"
+                spacing={2}
+                className={styles.bluebox}
+              >
                 <Grid item md={12}>
                   <div style={{ display: "flex" }}>
                     <img
-                      src={messagebox}
+                      src="/assets/images/regis-icon-2.svg"
                       alt=""
                       width={20}
                       height={20}
@@ -99,7 +151,7 @@ function Firstpage() {
                 <Grid item md={12} style={{ paddingTop: "0" }}>
                   <div style={{ display: "flex" }}>
                     <img
-                      src={vector}
+                      src="/assets/images/regis-icon-1.svg"
                       alt=""
                       width={20}
                       height={20}
@@ -116,41 +168,49 @@ function Firstpage() {
               </Grid>
               <Grid container maxWidth="sm">
                 <form>
-                  <div className="register-input-text">
+                  <div className={styles.register_input_text}>
+                    {/* {helpermsg.namehelpermsg != undefined ? } */}
                     <TextField
                       fullWidth
                       onChange={onnamechange}
                       id="standard-basic"
                       label="Nombre"
                       variant="standard"
+                      type="text"
+                      helperText={helpermsg.namehelpermsg}
+                      autoComplete=""
                     />
                   </div>
-                  <div className="register-input-text">
+                  <div className={styles.register_input_text}>
                     <TextField
                       fullWidth
                       onChange={onmothernamechange}
                       id="standard-basic"
                       label="Apellido materno"
                       variant="standard"
+                      type="text"
+                      helperText={helpermsg.mothernamehelpermsg}
                     />
                   </div>
-                  <div className="register-input-text">
+                  <div className={styles.register_input_text}>
                     <TextField
                       fullWidth
                       onChange={onlastnamechange}
                       id="standard-basic"
                       label="Apellido paterno"
                       variant="standard"
+                      type="text"
+                      helperText={helpermsg.lastnamehelpermsg}
                     />
                   </div>
-                  <div className="register-input-text">
+                  <div className={styles.register_input_text}>
                     <TextField
                       id="standard-basic"
                       label="RUT"
                       variant="standard"
                     />
                   </div>
-                  <div className="register-input-text">
+                  <div className={styles.register_input_text}>
                     <TextField
                       fullWidth
                       error={emailError}
@@ -158,10 +218,11 @@ function Firstpage() {
                       id="standard-basic"
                       label="Email"
                       variant="standard"
+                      type="email"
                       helperText={errormsg}
                     />
                   </div>
-                  <div className="register-input-text">
+                  <div className={styles.register_input_text}>
                     <Grid container maxWidth="sm" spacing={2}>
                       <Grid item md={3} style={{ paddingTop: "0" }}>
                         <Select
@@ -181,7 +242,7 @@ function Firstpage() {
                       </Grid>
                     </Grid>
                   </div>
-                  <div className="register-input-text">
+                  <div className={styles.register_input_text}>
                     <div style={{ display: "flex", alignItems: "flex-start" }}>
                       <Checkbox />
                       <div className="p1" style={{ paddingLeft: "10px" }}>

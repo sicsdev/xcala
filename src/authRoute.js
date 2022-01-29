e795b434b72f02f168b0ed039fbbf1c8d65b3fc3
sandeep kumar <sicsdev21@gmail.com>
2022-01-28 16:56:33 -0800

    latest change

--- src/authRoute.js
+++ src/authRoute.js
@@ -2,8 +2,8 @@ import "./App.scss";
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import "./Global/global.scss";
-import Public from "./Pages/Test/public";
-import Private from "./Pages/Test/private";
+import Public from "./Pages/test/public";
+import Private from "./Pages/test/private";
 function AuthRoute() {
   return (
     <>
